4b73caa6632ade7595c5872f310d4a5a2e32ac5f
unknown <Adebayoibrahim2468@gmial.com>
2021-10-10 23:17:49 +0100

    Updated Files

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.sidebar.title('About the App')
 st.sidebar.write("This is an app that transforms a horse to a zebra, or a zebra to a horse.")
 st.sidebar.write("The app consists of two GAN models, one converts picture of a horse to that of a zebra and the other converts picture of a zebra to that of a horse.")
 st.sidebar.write("Creepy, right...?")
-st.sidebar.write("The model is not perfect so there may be some mistakes.")
+st.sidebar.write("The model is not perfect so there may be some mistakes (The model converting horse to zebra is better than the one converting zebra to horse).")
 st.sidebar.write("And don't upload a picture that is not a horse or that is not a zebra, unless you want to see some weird stuff...")
 
 
